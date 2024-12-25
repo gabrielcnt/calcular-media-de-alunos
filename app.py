a1cf28b63bb8143869c1c01e1217079d0aeb6f07
gabriel vieira <gabrielcnt96@gmail.com>
2024-12-25 14:28:27 -0300

    script de calcular média finalizado

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-nome = str(input("Digite um nome: ")).strip().title()
-nota1 = float(input("Nota 1: "))
-nota2 = float(input("Nota 2: "))
-nota3 = float(input("Nota 3: "))
-nota4 = float(input("Nota 4: "))
+#Lista vazia de alunos
 
-def calcular_media():
+alunos = []
+
+while True:
+    nome = str(input("Digite um nome: ")).strip().title()
+    nota1 = float(input("Nota 1: "))
+    nota2 = float(input("Nota 2: "))
+    nota3 = float(input("Nota 3: "))
+    nota4 = float(input("Nota 4: "))
+
     media = (nota1 + nota2 + nota3 + nota4) / 4
-    if media >= 7:
-        print(f"A média de {nome} é {media:.1f}, APROVADO(A)")
-    else:
-        print(f"A média de {nome} é {media:.1f}, REPROVADO(A)")
-    
-calcular_media()
+    situacao = "APROVADO(A)" if media >= 7 else "REPROVADO(A)"
+
+#guardar dados dos alunos em um dicionario
+
+    aluno = {
+        "nome": nome,
+        "media": media,
+        "situacao": situacao
+    }
+
+    #adiciona aluno a lista
+    alunos.append(aluno)
+
+    # pergunta se quer continuar:
+    continuar = input("\nQuer adicionar outro aluno ? (S/N): ").upper()
+    if continuar != 'S':
+        break
+print("\n=== RESULTADO FINAL ===")
+
+for aluno in alunos:
+    print(f"A media de {aluno['nome']} é {aluno['media']:.1f}, {aluno['situacao']}")
