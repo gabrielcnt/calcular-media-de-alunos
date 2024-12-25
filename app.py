bf00298d567775a9fc1df0d7db64fbba6bc5662e
gabriel vieira <gabrielcnt96@gmail.com>
2024-12-25 00:31:16 -0300

    script de calculo de média

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ nota1 = float(input("Nota 1: "))
 nota2 = float(input("Nota 2: "))
 nota3 = float(input("Nota 3: "))
 nota4 = float(input("Nota 4: "))
+
+def calcular_media():
+    media = (nota1 + nota2 + nota3 + nota4) / 4
+    if media >= 7:
+        print(f"A média de {nome} é {media:.1f}, APROVADO(A)")
+    else:
+        print(f"A média de {nome} é {media:.1f}, REPROVADO(A)")
+    
+calcular_media()
